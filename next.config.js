ba0d36a6b2f6dce2d450d61f8ac03883a8d9ac9d
jx2hx1 <jx2hx1@gmail.com>
2022-08-31 20:34:03 +0900

    add: build test files

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  trailingSlash: true,
+  generateBuildId: () => "codecamp_deploy_08",
+  exportPathMap: () => ({
+    "/": { page: "/" },
+    "/boards": { page: "/boards" },
+    "/404": { page: "/404" },
+  }),
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

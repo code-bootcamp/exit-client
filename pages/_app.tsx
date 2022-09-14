ca693eec09dcdfd3168593072b95ab528a365a04
jx2hx1 <jx2hx1@gmail.com>
2022-09-14 17:33:06 +0900

    [Edit] globalStyles, breakPoints

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 import { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 import { Global } from "@emotion/react";
-import { globalStyles } from "../src/commons/styles/globalStyles";
 import Layout from "../src/components/commons/layout";
 import "antd/dist/antd.css";
 import ApolloSetting from "../src/commons/apollo";
+import { globalStyles } from "../src/commons/styles/globalStyles";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (

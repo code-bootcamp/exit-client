3586c26078d384c47c9bc66f7e12a153ad1546ec
yuniso <soyyn2206@gmail.com>
2022-09-21 00:07:09 +0900

    220921 edit

--- pages/exiting/[projectid]/index.tsx
+++
@@ -1,9 +0,0 @@
-import ProjectDetailContainenr from "../../../src/components/units/projects/detail/projectDetail.container";
-
-export default function ProjectDetailPage() {
-  return (
-    <>
-      <ProjectDetailContainenr />
-    </>
-  );
-}
